0279bc4c688e0282b02dd0746d6b109a133d80a7
unknown <alexa.glucio@gmail.com>
2025-10-09 17:55:57 -0600

    Add handling for all button press cases

--- IOs.c
+++ IOs.c
@@ -21,11 +21,32 @@ void IOinit()
 }
 
 void IOcheck()
-{
-    if (PORTBbits.RB4 == 0 && PORTAbits.RA4 == 0) 
+{   // Check combination PB1 + PB2 + PB3 first
+    if (PORTBbits.RB7 == 0 && PORTBbits.RB4 == 0 && PORTAbits.RA4 == 0) 
+    {
+        Disp2String("\033[2J\033[HAll PBs pressed\r");
+        while (PORTBbits.RB7 == 0 && PORTBbits.RB4 == 0 && PORTAbits.RA4 == 0)
+            _LATB9 = 1;
+    }
+    else if (PORTBbits.RB7 == 0 && PORTAbits.RA4 == 0)
+    {
+        Disp2String("\033[2J\033[HPB1 and PB3 event\r");
+        while (PORTBbits.RB7 == 0 && PORTAbits.RA4 == 0)
+        {
+            _LATB9 ^= 1;   // Blink LED while both buttons are pressed
+            delay_ms(500);
+        }
+    }
+    else if (PORTBbits.RB4 == 0 && PORTAbits.RA4 == 0) 
     {
         Disp2String("\033[2J\033[HPB2 and PB3 event\r");
         while (PORTBbits.RB4 == 0 && PORTAbits.RA4 == 0)
+            _LATB9 = 1;
+    }
+    else if (PORTBbits.RB7 == 0 && PORTBbits.RB4 == 0) 
+    {
+        Disp2String("\033[2J\033[HPB1 and PB2 event\r");
+        while (PORTBbits.RB7 == 0 && PORTBbits.RB4 == 0)
             _LATB9 = 1;
     }
     else if (PORTBbits.RB7 == 0) 
@@ -54,5 +75,9 @@ void IOcheck()
             _LATB9 ^= 1;
             delay_ms(3000);
         }
+    }
+    else
+    {
+        Disp2String("\033[2J\033[HNothing pressed\r");
     }
 }
